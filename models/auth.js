c2f14a0336d0f2be6365a01f777f50e6c23b93b0
JamiuKolawole04 <Jakko4real@gmail.com>
2023-06-10 09:49:14 +0100

    chore: adding required error messages

--- models/auth.js
+++ models/auth.js
@@ -1,62 +1,66 @@
-require("dotenv").config()
+require("dotenv").config();
 
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: [true, "please provide username"],
-        unique: true
+      type: String,
+      required: [true, "please provide username"],
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     country: {
-        type: String,
-        required: true
+      type: String,
+      required: [true, "country is required"],
     },
     img: {
-        type: String
+      type: String,
     },
     city: {
-        type: String,
-        required: true
+      type: String,
+      required: [true, "city is required"],
     },
     phone: {
-        type: String,
-        required: true,
+      type: String,
+      required: [true, "phone number is required"],
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     isAdmin: {
-        type: Boolean,
-        default: false
-    }
-},
-    { timestamps: true }
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
 UserSchema.pre("save", async function () {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-// creating tokens in mongoose schema 
+// creating tokens in mongoose schema
 UserSchema.methods.createJWT = function () {
-    return jwt.sign({ id: this._id, isAdmin: this.isAdmin }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_LIFETIME });
-}
+  return jwt.sign(
+    { id: this._id, isAdmin: this.isAdmin },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
+};
 
 // Comapring hashed passwords to be used one in the login route
 UserSchema.methods.comparePassword = async function (candidatePassword) {
-    const isMatch = await bcrypt.compare(candidatePassword, this.password);
-    return isMatch;
-}
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
 
 module.exports = model("User", UserSchema);
